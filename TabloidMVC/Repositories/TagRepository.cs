4e706e46d009557f4902881f9481977052133725
Joe <overtonje@gmail.com>
2021-01-04 10:36:45 -0600

    created basic tag repo

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+
+    }
