825361507e1b2aefb8fed241e75e310e0f3d7750
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-04 11:37:41 -0600

    comment repo created along with method for getting all comments

---
+++ TabloidMVC/Models/ViewModels/CommentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class CommentViewModel
+    {
+        public UserProfile User { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
