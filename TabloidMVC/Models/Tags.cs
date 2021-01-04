951675dceac6ebe712fcb9ad32ca6d5e89cefa8b
Joe <overtonje@gmail.com>
2021-01-04 10:19:51 -0600

    added tag model

---
+++ TabloidMVC/Models/Tags.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TabloidMVC.Models
+{
+    public class Tag
+    {
+    
+        public int Id {get; set;}
+
+        public string Name { get; set; }
+    
+    
+    
+    }
