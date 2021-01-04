8fc1d435755d11c7e0a2ad3d2df7789aeb75a2f5
joseph tidwell <joseph.tidwell09@gmail.com>
2021-01-04 11:50:55 -0600

    renamed branch from jt-tabloid-setup

--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 
 namespace TabloidMVC.Repositories
 {
     public interface ICommentRepository
     {
+        List<Comment> GetCommentsByPostId();
     }
 }
