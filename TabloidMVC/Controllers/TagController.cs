ed8a07716be44fa9011a0b960b0736b7a97ed42e
Joe <overtonje@gmail.com>
2021-01-04 11:00:42 -0600

    added nav bar item, created itagrepo and tag controller

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var tags = _tagRepository.GetAllTags();
+            return View(tags);
+        }
+    }
+}
