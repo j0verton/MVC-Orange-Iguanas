84cc30624c95d7861be50f96bd7f608459b80556
Patchrik <pklly92@gmail.com>
2021-01-06 12:50:03 -0600

    The deactivation process is working now. We're using a new user type Inactive.

--- TabloidMVC/Repositories/IUserProfileRepository.cs
+++ TabloidMVC/Repositories/IUserProfileRepository.cs
@@ -6,8 +6,8 @@ namespace TabloidMVC.Repositories
     public interface IUserProfileRepository
     {
         UserProfile GetByEmail(string email);
-
         public List<UserProfile> GetAllUserProfiles();
         UserProfile GetById(int id);
+        public void DeactiveUser(int id);
     }
 }
